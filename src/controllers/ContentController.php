0f87a834d6120afafe7dccf15b3f8f2e1c46a359
ChristopherPape <c.pape@pruesstecx.de>
2018-03-20 07:57:33 +0000

    Erster Versuch
    bin gespannt

---
+++ src/controllers/ContentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace HelloWorld\Controllers;
+
+
+use Plenty\Plugin\Controller;
+use Plenty\Plugin\Templates\Twig;
+
+class ContentController extends Controller{
+
+
+    public function sayHello(Twig $twig):string{
+
+        return $twig->render('Hello World :: content.hello');
+
+    }
+
+}
+
+?>
