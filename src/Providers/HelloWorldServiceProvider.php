69cafd6ef3db9a0b3d813365a7c9a1ab496f0969
ChristopherPape <c.pape@pruesstecx.de>
2018-03-21 12:29:32 +0000

    0.1.4
    0.1.4

--- src/Providers/HelloWorldServiceProvider.php
+++ src/Providers/HelloWorldServiceProvider.php
@@ -7,9 +7,6 @@ use Plenty\Plugin\ServiceProvider;
 
 class HelloWorldServiceProvider extends ServiceProvider{
 
-    /**
-     * Register the service provider.
-     */
 
     public function register(){
 
