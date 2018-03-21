a5238b694d26a4cc2861ea7a3d2b6525a3d3f9ee
ChristopherPape <c.pape@pruesstecx.de>
2018-03-21 07:22:52 +0000

    new Author
    CP

---
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace HelloWorld\Providers;
+
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+
+class HelloWorldRouteServiceProvider extends RouteServiceProvider{
+
+    public function map(Router $router){
+
+        $router->get('myplugin','HelloWorld\Controllers\ContentController@sayHello');
+    }
+}
+?>
